ed22b202170c092d2e7a95d9b20aba7dfa5951ff
Renan Gimenes Musardo <renan.gimenes@gmail.com>
2021-11-17 21:45:56 -0300

    Ajustado addProduct

--- Services/ProductService.cs
+++ Services/ProductService.cs
@@ -36,10 +36,25 @@ namespace AWS_API.Services
 
         internal Product Add(Product body)
         {
-            if (!context.Categories.Any(c => c.CategoryId == body.CategoryId))
-                throw new InvalidOperationException("Essa categoria não existe");
+            var p = new Product()
+            {
+                ProductName = body.ProductName,
+                Quantity = body.Quantity,
+                Category = body.Category,
+                Brand = body.Brand,
+                ListPrice = body.ListPrice
+            };
 
-            var data = context.Add(body);
+            var brand = context.Brands.Where(b => b.BrandName == p.Brand.BrandName).FirstOrDefault();
+            p.Brand = brand ?? throw new InvalidOperationException("Essa marca não existe");
+            
+            var category = context.Categories.Where(b => b.CategoryName == p.Category.CategoryName).FirstOrDefault();
+            p.Category = category ?? throw new InvalidOperationException("Essa categoria não existe");
+
+            p.CategoryId = category.CategoryId;
+            p.BrandId = brand.BrandId;
+
+            var data = context.Add(p);
             context.SaveChanges();
             return data.Entity;
         }
